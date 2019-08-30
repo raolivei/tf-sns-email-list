c9a9da6f555df3bf7ba61bab97a236601ea07e29
Rafael Oliveira <rafael@streetcontxt.com>
2019-08-30 15:58:36 -0400

    main.tf: Added equal sign for vars argument (terraform 0.12)

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "template_file" "cloudformation_sns_stack" {
   template = "${file("${path.module}/templates/email-sns-stack.json.tpl")}"
 
-  vars {
+  vars = {
     display_name  = "${var.display_name}"
     subscriptions = "${join("," , formatlist("{ \"Endpoint\": \"%s\", \"Protocol\": \"%s\"  }", var.email_addresses, var.protocol))}"
   }
